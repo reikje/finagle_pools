6a222d7fa44abc18a49c16d59edcf7e916a2f962
Reik Schatz <rschatz@ea.com>
2017-04-04 13:05:29 +0200

    added test case

--- build.sbt
+++ build.sbt
@@ -6,7 +6,9 @@ lazy val root =
       name := "finagle_pools",
       scalaVersion := "2.11.8",
       libraryDependencies ++= Seq(
-        "com.twitter" %% "finagle-core" % "6.42.0"
+        "com.twitter" %% "finagle-core" % "6.42.0",
+        "com.twitter" %% "finagle-http" % "6.42.0",
+        "com.twitter" %% "util-core" % "6.41.0"
       )
     )
 
--- src/main/scala/com/github/reikje/Test.scala
+++ src/main/scala/com/github/reikje/Test.scala
@@ -1,7 +1,37 @@
 package com.github.reikje
 
+import java.net.URL
+
+import com.twitter.conversions.time._
+import com.twitter.finagle.client.Transporter
+import com.twitter.finagle.factory.TimeoutFactory
+import com.twitter.finagle.http.Request
+import com.twitter.finagle.service.FailFastFactory.FailFast
+import com.twitter.finagle.{Http, param}
+import com.twitter.io.Buf
+import com.twitter.util.Await
+
 object Test {
   def main(args: Array[String]): Unit = {
-    println("works")
+    val url = new URL("https://github.com")
+
+    println(s"Creating client using inet!${url.getHost}:443")
+
+    val client = Http.client
+      .configured(param.Label("finagle_pools"))
+      .configured(TimeoutFactory.Param(10.seconds))
+      .configured(Transporter.ConnectTimeout(10.seconds))
+      .configured(FailFast(enabled = false))
+      .withTransport.tls(hostname = url.getHost)
+      .withTransport.verbose
+      .withSessionPool.minSize(1)
+      .withSessionPool.maxSize(2)
+      .withSessionPool.maxWaiters(250)
+      .newClient(s"inet!${url.getHost}:443")
+      .toService
+
+    val request = Request("https://github.com/twitter/finagle")
+    val response = Await.result(client(request))
+    println(new String(Buf.ByteArray.Owned.extract(response.content), "UTF-8"))
   }
 }
